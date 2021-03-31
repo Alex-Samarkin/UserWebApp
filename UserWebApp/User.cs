5bdd560db6f431330548974d92da0f0c7726d3b7
alex_ <alexsamarkinru@gmail.com>
2021-03-31 14:23:12 +0300

    Добавьте файлы проекта.

---
+++ UserWebApp/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserWebApp
+{
+    public class User
+    {
+        public int Id { get; set; } = 0;
+        public string SurName { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Middlename { get; set; } = "";
+        public int Age { get; set; }=18;
+        public string Address1 { get; set; } = "";
+        public string Address2 { get; set; } = "";
+
+    }
+}
